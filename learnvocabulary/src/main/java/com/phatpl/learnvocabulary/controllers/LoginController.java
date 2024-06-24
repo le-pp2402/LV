2b17a076ef541a08316b3a41f7faf1b2413ee1b0
le-pp2402 <lephiphatphat@gmail.com>
2024-06-24 14:42:11 +0700

    feat: auth

--- learnvocabulary/src/main/java/com/phatpl/learnvocabulary/controllers/LoginController.java
+++ learnvocabulary/src/main/java/com/phatpl/learnvocabulary/controllers/LoginController.java
@@ -4,30 +4,28 @@ package com.phatpl.learnvocabulary.controllers;
 import com.phatpl.learnvocabulary.dtos.request.LoginRequest;
 import com.phatpl.learnvocabulary.services.AuthService;
 import com.phatpl.learnvocabulary.utils.BuildResponse;
-import com.phatpl.learnvocabulary.utils.Logger;
-import jakarta.persistence.PersistenceContextType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequestMapping(value = "/login")
 public class LoginController {
     private final AuthService authService;
+
     @Autowired
     public LoginController(AuthService authService) {
         this.authService = authService;
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
     public ResponseEntity login(@RequestBody LoginRequest loginRequest) {
-
-        Logger.log("OK");
         try {
-            log.debug(authService.login(loginRequest).toString()    );
             return BuildResponse.ok(authService.login(loginRequest));
         } catch (Exception e) {
             return BuildResponse.badRequest(e.getMessage());
